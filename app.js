772946fa88bd1d66a817ac0668a1140bc08b65b1
jarrettluke84 <jarrettluke84@gmail.com>
2026-07-20 00:28:24 -0400

    Change image file extensions from .jpg to .jpeg

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // 1. DATA CONFIGURATION (Pre-populated with your 17 houseguests and images folder path)
 const initialHouseguests = [
-    { id: 1, name: "Barrett", drafter: "Jordyn", image: "./images/barrett.jpg", evicted: false },
-    { id: 2, name: "Taylor", drafter: "Emily", image: "./images/taylor.jpg", evicted: false },
-    { id: 3, name: "Mallory", drafter: "Gabby", image: "./images/mallory.jpg", evicted: false },
-    { id: 4, name: "La Trice", drafter: "Kait", image: "./images/latrice.jpg", evicted: false },
-    { id: 5, name: "Rome", drafter: "Hawk", image: "./images/rome.jpg", evicted: false },
-    { id: 6, name: "Yash", drafter: "Nick", image: "./images/yash.jpg", evicted: false },
-    { id: 7, name: "Dee", drafter: "Noah", image: "./images/dee.jpg", evicted: false },
-    { id: 8, name: "Lyric", drafter: "Luke", image: "./images/lyric.jpg", evicted: false },
-    { id: 9, name: "Chuk", drafter: "Luke", image: "./images/chuk.jpg", evicted: false },
-    { id: 10, name: "Rick", drafter: "Noah", image: "./images/rick.jpg", evicted: false },
-    { id: 11, name: "Jason", drafter: "Nick", image: "./images/jason.jpg", evicted: false },
-    { id: 12, name: "Ashley", drafter: "Hawk", image: "./images/ashley.jpg", evicted: false },
-    { id: 13, name: "Melody", drafter: "Kait", image: "./images/melody.jpg", evicted: false },
-    { id: 14, name: "Kamu", drafter: "Gabby", image: "./images/kamu.jpg", evicted: false },
-    { id: 15, name: "Haley", drafter: "Emily", image: "./images/haley.jpg", evicted: false },
-    { id: 16, name: "Drew", drafter: "Jordyn", image: "./images/drew.jpg", evicted: false },
-    { id: 17, name: "Angela", drafter: "Unchosen", image: "./images/angela.jpg", evicted: false }
+    { id: 1, name: "Barrett", drafter: "Jordyn", image: "./images/barrett.jpeg", evicted: false },
+    { id: 2, name: "Taylor", drafter: "Emily", image: "./images/taylor.jpeg", evicted: false },
+    { id: 3, name: "Mallory", drafter: "Gabby", image: "./images/mallory.jpeg", evicted: false },
+    { id: 4, name: "La Trice", drafter: "Kait", image: "./images/latrice.jpeg", evicted: false },
+    { id: 5, name: "Rome", drafter: "Hawk", image: "./images/rome.jpeg", evicted: false },
+    { id: 6, name: "Yash", drafter: "Nick", image: "./images/yash.jpeg", evicted: false },
+    { id: 7, name: "Dee", drafter: "Noah", image: "./images/dee.jpeg", evicted: false },
+    { id: 8, name: "Lyric", drafter: "Luke", image: "./images/lyric.jpeg", evicted: false },
+    { id: 9, name: "Chuk", drafter: "Luke", image: "./images/chuk.jpeg", evicted: false },
+    { id: 10, name: "Rick", drafter: "Noah", image: "./images/rick.jpeg", evicted: false },
+    { id: 11, name: "Jason", drafter: "Nick", image: "./images/jason.jpeg", evicted: false },
+    { id: 12, name: "Ashley", drafter: "Hawk", image: "./images/ashley.jpeg", evicted: false },
+    { id: 13, name: "Melody", drafter: "Kait", image: "./images/melody.jpeg", evicted: false },
+    { id: 14, name: "Kamu", drafter: "Gabby", image: "./images/kamu.jpeg", evicted: false },
+    { id: 15, name: "Haley", drafter: "Emily", image: "./images/haley.jpeg", evicted: false },
+    { id: 16, name: "Drew", drafter: "Jordyn", image: "./images/drew.jpeg", evicted: false },
+    { id: 17, name: "Angela", drafter: "Unchosen", image: "./images/angela.jpeg", evicted: false }
 ];
 
 // Persistent state via LocalStorage
